95656289ed8eb6196abdf4789fbe23feb1251ffa
marzu <marzuqazam06@gmail.com>
2024-09-26 14:22:14 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String partyAffil = "";
+        Scanner is = new Scanner(System.in);
+
+
+        System.out.print("Enter your party affiliation [D, R, I]: ");
+        partyAffil = is.nextLine();
+
+
+
+        if(partyAffil.equalsIgnoreCase("R"))
+        {
+            System.out.println("you get a republican elephant");
+
+        }else if(partyAffil.equalsIgnoreCase("D"))
+        {
+            System.out.println("you get a democratic donkey");
+        }
+        else if(partyAffil.equalsIgnoreCase("I"))
+        {
+            System.out.println("you get an independent man");
+        }
+        else
+        System.out.println("I don't know what party you belong to");
+        System.out.println("please input either a [D, R, or I]: ");
+    }
+}
